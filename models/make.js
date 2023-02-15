3bda97a0358183ed91c0f69af7f4ff45685d79b4
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-02-14 21:28:24 -0500

    add seeder for ford

---
+++ models/make.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Make extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Make.init({
+    name: DataTypes.STRING,
+    startyear: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Make',
+  });
+  return Make;
+};
---
+++ seeders/20230215021420-make.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+    const date = new Date()
+
+    await queryInterface.bulkInsert('Makes', [{
+      name: 'Ford',
+      startyear: 1901,
+      createdAt: date,
+      updatedAt: date,
+    }])
+
+  },
+
+  async down (queryInterface, Sequelize) {
+
+    await queryInterface.bulkDelete('Makes', null, {})
+  
+  }
+};
