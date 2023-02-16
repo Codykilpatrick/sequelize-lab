ad31434903a381ca1ad2efccb7499ddfc30eada7
Cody Kilpatrick <codykilpatrick1996@gmail.com>
2023-02-16 17:13:55 -0500

    fix bug with create controller

--- controllers/makes.js
+++ controllers/makes.js
@@ -2,7 +2,7 @@ const { Make } = require("../models")
 
 const create = async (req, res) => {
   try {
-    const cat = await Make.create(req.body)
+    const make = await Make.create(req.body)
     res.status(200).json(make)
   } catch (error) {
     res.status(500).json(error)
